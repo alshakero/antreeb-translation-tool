9b288e0d4e442466e482b60a1588ac42ac28bfba
Omar Alshaker <omar@omaralshaker.com>
2019-07-10 16:16:44 +0200

    Updates

---
+++ precache-manifest.9aa72a310da264af39b63f9c0c905609.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb15f6a5aaf3c96f29ad361a303604c5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6307fd2e5927e434e94",
+    "url": "/static/css/2.ca025b70.chunk.css"
+  },
+  {
+    "revision": "aed8296886b68f462339",
+    "url": "/static/css/main.7c414ba1.chunk.css"
+  },
+  {
+    "revision": "b6307fd2e5927e434e94",
+    "url": "/static/js/2.384bd4e7.chunk.js"
+  },
+  {
+    "revision": "aed8296886b68f462339",
+    "url": "/static/js/main.6c81bda3.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
